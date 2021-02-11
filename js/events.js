61181fb64d51fa007cfd2e2cb5855e981f93b9ce
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-11 16:36:28 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,15 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', () => {
+    alert('Hey!');
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
+
+getIt();
 
 });
